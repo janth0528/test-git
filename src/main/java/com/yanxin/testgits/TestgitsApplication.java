ab6142ce928e5a56f19a2c64e297dbfd52f432e2
janth0528 <janth0528@126.com>
2018-10-24 09:41:29 +0800

    first commit

---
+++ src/main/java/com/yanxin/testgits/TestgitsApplication.java
@@ -0,0 +1,12 @@
+package com.yanxin.testgits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestgitsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestgitsApplication.class, args);
+    }
+}
